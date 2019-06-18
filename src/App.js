ec5ec4f1a9156e5761a836aa33e657a9a310bc2e
Yan.Palyn <Kvinto1986@gmail.com>
2019-06-18 16:08:18 +0300

    code optimization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import store from './store';
 import { Provider } from 'react-redux';
@@ -8,30 +8,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'bootstrap/dist/js/bootstrap.js';
 
-import Home from './components/Home'
-import Header from './components/Header'
+import Home from './components/home/Home'
+import Header from './components/navigation/Header'
 import LoginCustomer from './components/login/LoginCustomer'
 import LoginTranslator from './components/login/LoginTranslator'
 import RegisterCustomer from './components/register/RegisterCustomer'
 
-const App = (props) => {
-  return (
-    <Provider store={ store }>
-      <Router >    
-          <Header history={props.history}/>
-          <main className="mainContext">
-            <div className="container cheifAppContainer">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/loginCustomer" component={LoginCustomer} />
-              <Route exact path="/loginTranslator" component={LoginTranslator} />
-              <Route exact path="/registerCustomer" component={RegisterCustomer} />
-            </Switch>
-            </div>
-          </main>
-      </Router>
-    </Provider>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <main className="mainContext">
+                        <Header/>
+                        <div className="container cheifAppContainer">
+                            <Switch>
+                                <Route exact path="/" component={Home}/>
+                                <Route exact path="/loginCustomer" component={LoginCustomer}/>
+                                <Route exact path="/loginTranslator" component={LoginTranslator}/>
+                                <Route exact path="/registerCustomer" component={RegisterCustomer}/>
+                            </Switch>
+                        </div>
+                    </main>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
--- src/components/register/RegisterCustomer.js
+++ src/components/register/RegisterCustomer.js
@@ -1,72 +1,93 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const RegisterCustomer = props => {
-    return (
-        <div className="registerCustomer">
-            <div className="container">
-                <h2>Registration</h2>
-                <p className="text-info">Customer</p>
-                <form>
-                    <div className="form-group">
-                        <input
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        className="form-control"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input
-                        type="text"
-                        placeholder="Name"
-                        name="name"
-                        className="form-control"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input
-                        type="text"
-                        placeholder="Credit card number"
-                        name="cardNumber"
-                        className="form-control"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        className="form-control"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input
-                        type="password"
-                        placeholder="Confirm Password"
-                        name="password_confirm"
-                        className="form-control"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <div className="form-check form-check-inline">
-                            <label className="form-check-label checkingUnit">I read license</label>
-                            <input className="form-check-input" type="checkbox" />
+class RegisterCustomer extends Component {
+    state = {
+        name: "",
+        email: "",
+        password: '',
+        password_confirm: '',
+        creditCard:'',
+        errors: {},
+    };
+
+    handleInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    };
+
+    render() {
+        return (
+            <div className="registerCustomer">
+                <div className="container">
+                    <h2>Registration</h2>
+                    <p className="text-info">Customer</p>
+                    <form>
+                        <div className="form-group">
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                name="email"
+                                className="form-control"
+                                onChange={this.handleInputChange}
+                                value={this.state.email}
+                            />
                         </div>
-                    </div>
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-primary">
-                            Register
-                        </button>
-                    </div>
-                    <div className="form-group">
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                name="name"
+                                className="form-control"
+                                onChange={this.handleInputChange}
+                                value={this.state.name}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                placeholder="Credit card number"
+                                name="cardNumber"
+                                className="form-control"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                className="form-control"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                placeholder="Confirm Password"
+                                name="password_confirm"
+                                className="form-control"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <div className="form-check form-check-inline">
+                                <label className="form-check-label checkingUnit">I read license</label>
+                                <input className="form-check-input" type="checkbox"/>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary">
+                                Register
+                            </button>
+                        </div>
+                        <div className="form-group">
                         <span className="text-primary">
                             Forget the password
                         </span>
-                    </div>
-                </form>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default RegisterCustomer
