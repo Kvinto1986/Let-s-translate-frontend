4c3d173c4a69024719deedfea21e3a76e33ba476
Maksim Yepifanau <Maksim.Yepifanau@itechart-group.com>
2019-06-20 12:07:13 +0300

    some links

--- src/components/navigation/LinkGroup.js
+++ src/components/navigation/LinkGroup.js
@@ -1,7 +1,31 @@
 import React from 'react'
 
 const LinkGroup = ({role}) => {
-    return null
+
+    const adminLinks = [
+        {title: 'Register translators', route: '/registerTranslator'},
+        {title: 'Edit tarifs', route: '/venRegistration'},
+        {title: 'Vendor Registration', route: '/venRegistration'},
+        {title: 'Vendor Registration', route: '/venRegistration'}
+    ]
+
+    const customerLinks = [
+        {title: 'Dashboard', route: '/dashboard'},
+        {title: 'New Tasks', route: '/tasks'},
+        {title: 'My Transactions', route: '/transactions'},
+        {title: 'Messages', route: '/messages'}
+    ]
+
+    const translatorLinks = [
+        {title: 'Vendor Registration', route: '/venRegistration'},
+        {title: 'Vendor Registration', route: '/venRegistration'},
+        {title: 'Vendor Registration', route: '/venRegistration'},
+        {title: 'Vendor Registration', route: '/venRegistration'}
+    ]
+
+    return (
+        null
+    )
 }
 
 export default LinkGroup
