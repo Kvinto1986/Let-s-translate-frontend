6ea5736c05c73d903f59c19c625b03fa82c76e02
Yan.Palyn <Kvinto1986@gmail.com>
2019-07-11 12:03:53 +0300

    restart project

--- serverConfig.js
+++
@@ -1,2 +0,0 @@
-const server = 'https://localhost:5000/';
-export default server
--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    apiKey: 'AIzaSyDeIBoTk8_gUbkupM2RbmoPT1--3-qOlKU',
-    authDomain: 'warcraft4game.firebaseapp.com',
-    databaseURL: 'https://warcraft4game.firebaseio.com',
-    projectId: 'warcraft4game',
-    storageBucket: 'warcraft4game.appspot.com'
-};
-
-export default config
