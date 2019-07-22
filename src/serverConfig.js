19357368f8112bd8e44710a35af485bef745b80c
Yan.Palyn <Kvinto1986@gmail.com>
2019-07-22 11:41:48 +0300

    for heroku

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,2 +1,3 @@
-const server = 'https://letstranslate-back.herokuapp.com/';
+//const server = 'https://letstranslate-back.herokuapp.com/';
+const server = 'https://localhost:5000/';
 export default server
